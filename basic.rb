64e79de89b71dc2318672c639112b177344f056f
thealexcesar <thealexcesar@gmail.com>
2022-03-06 18:48:47 -0300

    ML

---
+++ basic.rb
@@ -0,0 +1,11 @@
+age = 34
+first_name = 'Alex'
+last_name = 'Cesar da Silva'
+full_name = first_name + last_name
+
+puts "#{full_name} has #{age} years old" # Interpolacao
+puts(full_name + ' has ' + age + ' years old') # concatenacao
+
+console.log(`${full_name} has ${age} years old`)
+
+printf('%s has %i age year', full_name, age)
---
+++ ruby.md
@@ -0,0 +1,10 @@
+```ruby
+49.is_a?(Integer)
+(1..5)include?(3)
+/ell/ =~ 'Hello'
+```
+```ruby
+age = 35
+puts "Alex has #{age} years old"
+puts ("Alex has" + age + "years old")
+```
